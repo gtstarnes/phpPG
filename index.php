5e402cbe28a27a0ba2012a7d947f056801fb7453
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-02 12:44:44 -0500

    convert to html

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
-$name = "Jimmy";
-$otherName = &$name;
-$name = "Chaz";
-echo "Hello, {$name}";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    <?php
+        $name = "Jimmy";
+        $otherName = &$name;
+        $name = "Chaz";
+        echo "Hello, {$name}";
+    ?>
+</h1>
+
+</body>
+</html>
