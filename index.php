6b4fa8b853509a79090d78bb7761ccbfdc9d9957
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-08 13:05:43 -0500

    display constant

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-
+    <h1>
+        Hello, 
+        <?php 
+            define("FIRSTNAME", "Jeff");
+            echo FIRSTNAME;
+        ?>
+    </h1>
 </body>
 </html>
